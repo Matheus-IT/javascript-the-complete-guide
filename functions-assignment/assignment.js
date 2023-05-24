8f3a99fe3d5e0f0fe68620dd51fd330b0f12ffc2
matheus costa <matheuscosta.tech@gmail.com>
2023-05-23 22:59:21 -0300

    Functions practice

--- functions-assignment/assignment.js
+++ functions-assignment/assignment.js
@@ -1,5 +1,11 @@
-function sayHello(name) {
-  console.log('Hi ' + name);
-}
+const sayHello = () => 'hi matheus';
 
 sayHello();
+
+function checkInput(cb, ...arguments) {
+    if (arguments.length === 0) {
+        cb();
+    } 
+}
+
+checkInput(() => 'hello', 'test', 'ing', 'funcs');
