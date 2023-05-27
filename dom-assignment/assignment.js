9dd171ebf312ab61c213248bf7425c7d62830155
matheus costa <matheuscosta.tech@gmail.com>
2023-05-26 22:04:56 -0300

    Assignment solved

--- dom-assignment/assignment.js
+++ dom-assignment/assignment.js
@@ -6,3 +6,10 @@ console.log('task1 - second method', task1);
 
 task1.style.backgroundColor = 'black';
 task1.style.color = 'white';
+
+let task2 = document.querySelector('title');
+task2 = document.head.querySelector('title');
+task2.innerText = 'Assignment - Solved!';
+
+let task3 = document.querySelector('h1');
+task3.innerHTML = task2.innerText;
