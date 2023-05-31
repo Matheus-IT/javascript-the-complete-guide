7b4c4ed401ba70815922c8d0d4a9c9688321cd56
matheus costa <matheuscosta.tech@gmail.com>
2023-05-30 22:27:44 -0300

    Handling backdrop

--- favorite-movies-project/assets/scripts/app.js
+++ favorite-movies-project/assets/scripts/app.js
@@ -1,5 +1,19 @@
 const addMovieModal = document.getElementById('add-modal');
+
 const startAddMovieButton = document.getElementById('addMovieButton');
-startAddMovieButton.onclick = function (e) {
+startAddMovieButton.onclick = handleAddMovieClicked;
+
+const cancelAddMovie = document.getElementById('cancelAddMovie');
+cancelAddMovie.onclick = handleAddMovieClicked;
+
+const backdrop = document.getElementById('backdrop');
+backdrop.onclick = handleAddMovieClicked;
+
+function handleAddMovieClicked(e) {
     addMovieModal.classList.toggle('visible');
-};
+    toggleBackdrop();
+}
+
+function toggleBackdrop() {
+    backdrop.classList.toggle('visible');
+}
